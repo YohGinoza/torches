7dc3b32e604edd90bcc40c810124a863f63f5f41
asinma2ch <43317719+asinma2ch@users.noreply.github.com>
2019-11-10 17:08:41 +0700

    removed ..cpp files

--- torches/BeastNu.h
+++ torches/BeastNu.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Monster.h"
+#include <stdlib.h>
 
 class BeastNu :
 	public Monster<int>
@@ -12,3 +13,24 @@ public:
 	virtual void GenerateSequence() override;	// randomize a new set of sequence : sequence is 1-9
 };
 
+BeastNu::BeastNu()
+{
+}
+
+BeastNu::BeastNu(int hp, int atk) {
+	this->Hp = hp;
+	this->AttackDamage = atk;
+	this->isAlive = true;
+}
+
+BeastNu::~BeastNu()
+{
+}
+
+void BeastNu::GenerateSequence() {
+	for (int i = 0; i < m_SequenceKeeper.GetRange(); i++) {
+		int randNum = (rand() % 9) + 1;
+		m_SequenceKeeper.SetSequenceAt(i, randNum);
+	}
+
+}
--- torches/Monster.h
+++ torches/Monster.h
@@ -14,10 +14,46 @@ public:
 	int GetAttackDamage() const;
 	bool GetAliveStatus() const;
 	SequenceKeeper<T> m_SequenceKeeper;
-	virtual void GenerateSequence() = 0 override;	// randomize a new set of sequence
+	virtual void GenerateSequence() = 0;	// randomize a new set of sequence
 protected:
 	int Hp;
 	int AttackDamage;
 	bool isAlive;
 };
 
+template <class T>
+Monster<T>::Monster()
+{
+}
+
+template <class T>
+Monster<T>::~Monster()
+{
+}
+
+template <class T>
+Monster<T>::Monster(int hp, int atk) {
+	Hp = hp;
+	AttackDamage = atk;
+	isAlive = true;
+}
+
+template <class T>
+int Monster<T>::GetHp() const {
+	return Hp;
+}
+
+template <class T>
+int Monster<T>::GetAttackDamage() const {
+	return AttackDamage;
+}
+
+template <class T>
+bool Monster<T>::GetAliveStatus() const {
+	return isAlive;
+}
+
+template <class T>
+void Monster<T>::GenerateSequence() {
+
+}
