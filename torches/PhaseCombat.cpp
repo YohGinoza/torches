be7fbf848dbbc865654532912ab6d418f840ebbd
Natnaree <fafuang@gmail.com>
2019-11-13 17:16:23 +0700

    no message

--- torches/PhaseCombat.cpp
+++ torches/PhaseCombat.cpp
@@ -1,9 +1,16 @@
 #include "PhaseCombat.h"
+#include "Game.h"
 
 PhaseCombat* PhaseCombat::s_Instance = 0;
 
 PhaseCombat::PhaseCombat()
 {
+
+}
+
+PhaseCombat::PhaseCombat(Monster* enemy)
+{
+
 }
 
 
@@ -13,7 +20,17 @@ PhaseCombat::~PhaseCombat()
 
 void PhaseCombat::OnUpdate(float dt)
 {
-
+	//get input
+	if (Game::getInput()->getKey(0) != NULL)
+	{
+		//if correct continue
+		if (c_enemy->m_SequenceKeeper.GetSequence()[0] == Game::getInput()->getKey(0))
+		{
+			std::cout << "Damage" << std::endl;
+		}
+	}
+	//if incorrect stop
+	//calculate the damage
 }
 
 PhaseCombat* PhaseCombat::GetInstance()
@@ -21,6 +38,15 @@ PhaseCombat* PhaseCombat::GetInstance()
 	if (s_Instance == 0)
 	{
 		s_Instance = new PhaseCombat();
+	}
+	return s_Instance;
+}
+
+PhaseCombat* PhaseCombat::GetInstance(Monster* enemy)
+{
+	if (s_Instance == 0)
+	{
+		s_Instance = new PhaseCombat(enemy);
 	}
 	return s_Instance;
 }
