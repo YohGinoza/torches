ec4ae09311a2d97f7f9b20d9a197368bf3581d1b
phillip2174 <47382992+phillip2174@users.noreply.github.com>
2019-11-15 23:25:45 +0700

    Merged header & cpp files: Monster class & Item class

--- torches/Item.h
+++ torches/Item.h
@@ -6,3 +6,21 @@ public:
 	~Item();
 	virtual void OnRead() = 0;
 };
+
+class Healer :
+	public Item
+{
+public:
+	Healer();
+	~Healer();
+	virtual void OnRead() override;
+};
+
+class Lighter :
+	public Item
+{
+public:
+	Lighter();
+	~Lighter();
+	virtual void OnRead() override;
+};
