b31375d85cebac55405db6b51fa5a6afcd684836
phillip2174 <47382992+phillip2174@users.noreply.github.com>
2019-11-18 21:47:48 +0700

    Tweak: Room constructor Added: boolean fuction to check have monster and torch or not

--- torches/Room.cpp
+++ torches/Room.cpp
@@ -1,24 +1,42 @@
 #include "Room.h"
+#include <stdlib.h>
 
 
 
 Room::Room()
 {
 	this->m_RoomId = 0;
-	this->width = 0;
-	this->height = 0;
-	this->door = 0;
+	this->width = 73;
+	this->height = 57;
+	this->door = 1;
+	this->torch = false;
+	this->spawnMon = false;
+	for (int i = 0; i < monster.size(); i++){
+		monster[i] = nullptr;
+	}
 	
 }
 
-Room::Room(int id, int width, int height, int door, std::vector<Entity*> monsterBucket) {
+Room::Room(int id, int width, int height, int door, bool torch, bool spawnMon, std::vector<Entity*> monsterBucket) {
 	this->m_RoomId = id;
 	this->width = width;
 	this->height = height;
 	this->door = door;
+	this->torch = torch;
+	this->spawnMon = spawnMon;
+	for (int i = 0; i < monsterBucket.size(); i++) {
+		monster[i] = monsterBucket[i];
+	}
 
 }
 
+bool Room::haveTorch() {
+	return this->torch;
+}
+
+bool Room::haveMon() {
+	return this->spawnMon;
+}
 
 Room::~Room()
 {
