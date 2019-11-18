b9380e5621d04e3c7fd52dddd197129e0aeb573e
phillip2174 <47382992+phillip2174@users.noreply.github.com>
2019-11-18 22:52:40 +0700

    Created: Time class & DeltaTime functions

---
+++ torches/Time.cpp
@@ -0,0 +1,35 @@
+#include "Time.h"
+
+Time* Time::s_Instance = nullptr;
+
+Time::Time()
+{
+	deltaTime = 0.0f;
+	oldTime = 0.0f;
+
+	if (s_Instance == nullptr) {
+		s_Instance = new Time();
+	}
+	else {
+		delete this;
+		return;
+	}
+
+}
+
+Time* Time::GetInstance() {
+	return s_Instance;
+}
+
+void Time::OnUpdate() {
+	deltaTime = clock() - oldTime;
+	oldTime = clock();
+}
+
+float Time::GetDeltaTime() {
+	return deltaTime;
+}
+
+Time::~Time()
+{
+}
